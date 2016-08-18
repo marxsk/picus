7dbee4fbbf720e04c67fa383424532c1672395c3
Marek 'marx' Grác <mgrac@redhat.com>
2016-08-18 13:29:23 +0200

    [feat] Add component for items in Menu/Tree

---
+++ app/components/menu-item.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  title: undefined,
+  status: undefined,
+  componentId: undefined,
+  isCollapsable: false,
+  isCollapsed: true,
+  isChecked: false,
+  onClickAction: undefined,
+
+  watchChecked: function() {
+    this.attrs.onCheckAction(this.get('componentId'), this.get('isChecked'));
+  }.observes('isChecked'),
+
+  actions: {
+    onSelectItem() {
+      this.attrs.onClickAction(this.get('componentId'));
+      this.toggleProperty('isCollapsed');
+    },
+    onSelectCollapse() {
+      this.toggleProperty('isCollapsed');
+    }
+  },
+});
