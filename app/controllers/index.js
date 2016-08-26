400ed6a493408384467138873b6e05c683429b90
Marek 'marx' Grác <mgrac@redhat.com>
2016-08-26 14:41:27 +0200

    [feat/no-test] Add computed property that check if selected item is 'node'

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,6 +4,14 @@ export default Ember.Controller.extend({
   selectedComponentId: undefined,
   selectedComponent: undefined,
 
+  isSelectedNode: Ember.computed('selectedComponent', function() {
+    if (this.get('selectedComponent')) {
+      return (this.get('selectedComponent').toString().indexOf('@model:node::') > -1);
+    } else {
+      return false;
+    }
+  }),
+
   actions: {
     onClick: function(component, componentId) {
       this.set('selectedComponentId', componentId);
