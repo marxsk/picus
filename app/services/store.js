2765cbaef1f982e49c3c73266feca337e4262947
Marek 'marx' Grác <mgrac@redhat.com>
2016-08-31 13:26:08 +0200

    [feat/no-test] Updating store
    Tests are missing because currently, we do not have working mock
    back-end. So instead of deleting attributes, they should switch from
    DELETING, ADDING to an original state.

--- app/services/store.js
+++ app/services/store.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Store.extend({
   /** Reload all data from backend
@@ -18,8 +19,41 @@ export default DS.Store.extend({
     res.then(function(response) {
       var modelClass = store.modelFor('cluster');
       var normalized = ser.normalizeSingleResponse(store, modelClass, response);
-      store.push(normalized);
 
+      const generateId = function(json, parent) {
+        return Ember.get(json, parent + 'type') + '::' + Ember.get(json, parent + 'id');
+      };
+
+      if (generateId(normalized, 'data.0.') === 'undefined::undefined') {
+        // response does not contain relevant data, we should skip the processing
+        // @todo: error/warning?
+        return;
+      }
+
+      const knownIds = Ember.A();
+      // add ID of cluster itself - we are working with just one cluster in response
+      knownIds.addObject(generateId(normalized, 'data.0.'));
+
+      // add information of all objects that are included with cluster response
+      if (Ember.get(normalized, 'included') !== undefined) {
+        Ember.get(normalized, 'included').forEach(function(item) {
+          knownIds.addObject(generateId(item, ''));
+        });
+      }
+
+      // remove records which no longer exists on backend from store for every used model
+      ['node', 'resource', 'attribute'].forEach(function(modelName) {
+          store.peekAll(modelName).forEach(function(item) {
+            if (!knownIds.contains(modelName + '::' + item.get('id'))) {
+              item.deleteRecord();
+            } else {
+              // reset flags (isDeleted,...) to 'clean' state
+              item.rollbackAttributes();
+            }
+          });
+      });
+
+      store.push(normalized);
     }, function(error) {
       alert(error);
     });
