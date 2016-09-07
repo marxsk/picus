30629bc9675aa554efd71991cb3b479ff08c7fb5
Marek 'marx' Grác <mgrac@redhat.com>
2016-09-07 09:54:23 +0200

    [feat] Add checkbox to attr-single

--- app/components/attr-single.js
+++ app/components/attr-single.js
@@ -3,4 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   attribute: undefined,
   onDeleteAction: undefined,
+  onCheckAction: undefined,
+
+  isChecked: false,
+
+  watchChecked: function() {
+    this.attrs.onCheckAction(this.get('attribute'), this.get('isChecked'));
+  }.observes('isChecked'),
 });
--- tests/integration/components/attr-single-test.js
+++ tests/integration/components/attr-single-test.js
@@ -20,9 +20,9 @@ test('render usual attribute', function(assert) {
   this.set('onDelete', () => { });
   this.render(hbs`{{attr-single attribute=myAttr onDeleteAction=(action onDelete)}}`);
 
-  assert.equal(this.$('tr td:nth-child(1)').text(), 'foo');
-  assert.equal(this.$('tr td:nth-child(2)').text(), 'bar');
-  assert.equal(this.$('tr td:nth-child(3)').text().trim(), 'DELETE');
+  assert.equal(this.$('tr td:nth-child(2)').text(), 'foo');
+  assert.equal(this.$('tr td:nth-child(3)').text(), 'bar');
+  assert.equal(this.$('tr td:nth-child(4)').text().trim(), 'DELETE');
 });
 
 test('render attribute after marking as deleted', function(assert) {
@@ -39,7 +39,7 @@ test('render attribute after marking as deleted', function(assert) {
   this.set('onDelete', () => { });
   this.render(hbs`{{attr-single attribute=myAttr onDeleteAction=(action onDelete)}}`);
 
-  assert.equal(this.$('tr td:nth-child(3)').text().trim(), '[DELETING]');
+  assert.equal(this.$('tr td:nth-child(4)').text().trim(), '[DELETING]');
 });
 
 test('render new attribute which is not in backend yet', function(assert) {
@@ -56,7 +56,7 @@ test('render new attribute which is not in backend yet', function(assert) {
   this.set('onDelete', () => { });
   this.render(hbs`{{attr-single attribute=myAttr onDeleteAction=(action onDelete)}}`);
 
-  assert.equal(this.$('tr td:nth-child(3)').text().trim(), '[ADDING]');
+  assert.equal(this.$('tr td:nth-child(4)').text().trim(), '[ADDING]');
 });
 
 test('test if action to delete attribute was called', function(assert) {
@@ -76,5 +76,26 @@ test('test if action to delete attribute was called', function(assert) {
   this.render(hbs`{{attr-single attribute=myAttr onDeleteAction=(action onAttrDelete)}}`);
   const $button = this.$('.delete-attr');
   assert.equal($button.length, 1, 'There is just one "button" to click on');
+  $button.click();
+});
+
+test('test if change on checkbox call action', function(assert) {
+  assert.expect(2);
+
+  const attr = Ember.Object.create({
+    id: 123,
+    isDeleted: false,
+    key: 'foo',
+    value: 'bar',
+  });
+
+  this.set('myAttr', attr);
+  this.set('onAttrDelete', () => { });
+  this.set('onCheck', (attr) => {
+    assert.equal(123, attr.get('id'));
+  });
+  this.render(hbs`{{attr-single attribute=myAttr onCheckAction=(action onCheck) onDeleteAction=(action onAttrDelete)}}`);
+  const $button = this.$(':checkbox');
+  assert.equal($button.length, 1, 'There is just one "checkbox" to click on');
   $button.click();
 });
