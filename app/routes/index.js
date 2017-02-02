4ca47a8cbeeb808126f87b3a75e457aa83116124
Marek 'marx' Grác <mgrac@redhat.com>
2017-02-02 16:15:17 +0100

    [refactor] Replace obsolete version of / with new navigation

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  queryParams: {
-    filterString: {
-      as: 's',
-      replace: true
-    }
-  },
-  actions: {
-    pageRefresh() {
-      this.refresh();
-    }
-  },
-  model({search}) {
-    if (!search) {
-      this.store.reloadData();
-    }
-
-    return this.store.peekAll('cluster');
-  }
 });
