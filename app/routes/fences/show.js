4907fb8d37667d494674ba4975907431a5aca927
Marek 'marx' Grác <mgrac@redhat.com>
2017-03-02 18:36:11 +0100

    [feat] Improve working with fence agent properties that are boolean
    * [1,true,'on','true'] should be treat as a single value
    * if default value is TRUE (any of previous value) then it cannot be disabled in UI

--- app/routes/fences/show.js
+++ app/routes/fences/show.js
@@ -1,10 +1,29 @@
 import Ember from 'ember';
+const { RSVP } = Ember;
 
 export default Ember.Route.extend({
   modelForm: {},
+  metadata: undefined,
 
-  beforeModel() {
-    return this.store.reloadData();
+  beforeModel(transition) {
+    const fenceId = transition.state.params['fences.show'].fence_id;
+
+    if (fenceId == '') {
+      return this.store.reloadData();
+    }
+
+    return new RSVP.Promise((resolve, reject) => {
+      this.store.reloadData().then((response) => {
+        this.store.getAgentMetadata('fence', 'stonith:' + this.store.peekRecord('fence', fenceId).get('agentType')).then((resp) => {
+          this.set('metadata', resp);
+          resolve();
+        }, (xh) => {
+          // @todo: proper error handling
+        });
+      }, (xhr) => {
+        // @todo: proper error handling
+      });
+    });
   },
 
   model(params) {
@@ -18,13 +37,35 @@ export default Ember.Route.extend({
       });
     }
 
+    // normalize (1|on|true|yes) => 'true' for boolean options
+    // this should be in serialization (?) but we need agent metadata to do it
+
+    // -> in metadata
+    this.get('metadata.parameters').forEach((o) => {
+      if (o.type === 'boolean') {
+        if (['1', 'on', true, 'yes'].includes(o.default)) {
+          o.default = 'true';
+        };
+      }
+    });
+
+    // -> in the properties
     fence.get('properties').forEach((item) => {
+      console.log('meta');
+      this.get('metadata.parameters').forEach((o) => {
+        if (o.name === item.get('name')) {
+          if (['1', 'on', true, 'yes'].includes(item.get('value'))) {
+            item.set('value', 'true');
+          };
+        }
+      });
+
       this.set('modelForm.' + item.get('name'), item.get('value'));
     });
 
     return Ember.RSVP.hash({
       params: params,
-      metadata: this.store.getAgentMetadata('fence', 'stonith:' + this.store.peekRecord('fence', params.fence_id).get('agentType')),
+      metadata: this.get('metadata'),
       formData: this.get('modelForm'),
       cluster: this.store.peekAll('cluster'),
       selectedFence: this.store.filter('fence', (item) => { return item.id === params.fence_id; }),
