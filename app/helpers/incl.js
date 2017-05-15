5ddbadeb1690cc78c2f744fccf738e7816364e80
Marek 'marx' Grác <mgrac@redhat.com>
2017-05-15 23:34:04 +0200

    [fix] Remove logging to console

--- app/helpers/incl.js
+++ app/helpers/incl.js
@@ -2,11 +2,9 @@ import Ember from 'ember';
 
 export function incl(params/*, hash*/) {
   if (!params[1]) {
-    console.log('incl - 1');
     return true;
   }
   if (!params[0]) {
-    console.log('incl - 2');
     return true;
   }
   return (params[0].includes(params[1]));
