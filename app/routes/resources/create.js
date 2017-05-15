abf2d58c4393735fe142ce97e5311871d889f7be
Marek 'marx' Grác <mgrac@redhat.com>
2017-05-15 13:11:09 +0200

    [fix] Transition from created resource to index page

--- app/routes/resources/create.js
+++ app/routes/resources/create.js
@@ -64,7 +64,7 @@ export default Ember.Route.extend({
         properties: form.get('changes'),
       }, 'create');
 
-      this.transitionTo('resources.show', '');
+      this.transitionTo('resources.index');
     }
   }
 });
