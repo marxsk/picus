752248030ad298cbf15edfaeda5b5d8b2487a626
Marek 'marx' Grác <mgrac@redhat.com>
2017-06-20 12:48:51 +0200

    [feat] Add support for 'secret' type in resource metadata

--- app/components/agent-properties-sections.js
+++ app/components/agent-properties-sections.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  formObject: undefined,
+
+  actions: {
+    showSecret(property, oldValue) {
+        // In order to obtain current value from form, we need to do a work-around
+        // If no-one touches the field -> return value from initializiation
+        // If the field was modified -> (work-around) updateSecretLength is executed and
+        // it sets us a formObject that cannot be accessed directly. And then a value is
+        // obtained from the form property.
+
+        let secret = '';
+        if (this.get('formObject') && (this.get('formObject').get(property) !== undefined)) {
+          secret = this.get('formObject').get(property);
+        } else {
+          secret = oldValue;
+        }
+
+        alert(`The secret is '${secret}'`);
+    },
+    updateSecretLength(object, property, value) {
+      this.formObject = object;
+      Ember.set(object, property, value);
+    }
+  }
 });
