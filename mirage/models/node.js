d815db04a92e167d95c2ea9e019409a0358f9f7b
Marek 'marx' Grác <mgrac@redhat.com>
2017-10-02 10:43:45 +0200

    [refactor] Models are now generated from ember-data

--- mirage/models/node.js
+++
@@ -1,6 +0,0 @@
-import { Model, hasMany } from 'ember-cli-mirage';
-
-export default Model.extend({
-  nodeAttributes: hasMany('attribute', {inverse: 'unused1'}),
-  nodeUtilizationAttributes: hasMany('attribute', {inverse: 'unused2'}),
-});
