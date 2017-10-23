fee3050a4c2e3873c3542d28279867e6ba61a441
Marek 'marx' Grác <mgrac@redhat.com>
2017-10-23 11:59:09 +0200

    [fix] Remove double rendering in ember-power-select & dynamic-fields

--- app/components/dynamic-fields.js
+++ app/components/dynamic-fields.js
@@ -5,6 +5,7 @@ export default Ember.Component.extend({
   key: undefined,
   elementPrefix: 'dynamic',
 
+  source: undefined,
   /**
    * The original idea was to use 'source' as the basis for changes.
    * What is good idea but as we work with undefined value, we want
@@ -12,24 +13,23 @@ export default Ember.Component.extend({
    * to split it into 'src' and 'key' what offers us initialization without
    * changing address of 'src'.
    */
-  init() {
-      this._super(...arguments);
-      if (!(this.get('key') in this.get('src'))) {
+  actions: {
+    update: function(target, objectName, value) {
+      const re = /(?:_([^_]+))?$/;
+      let lastIndex = parseInt(re.exec(this.get('source.lastObject.name'))[1]);
+
+      if (this.get('source') === undefined) {
         Ember.Logger.debug('dynamic-fields: Source was undefined, creating the array with empty item');
         this.set('source', Ember.A());
+
         this.get('src').set(this.get('key'), this.get('source'));
         this.get('source').pushObject(Ember.Object.create({
           name: `${this.get('elementPrefix')}_0`,
           value: Ember.A(),
         }));
-      }
-  },
 
-  actions: {
-    update: function(target, objectName, value) {
-      const re = /(?:_([^_]+))?$/;
-      const lastIndex = parseInt(re.exec(this.get('source.lastObject.name'))[1]);
-      const currentIndex = parseInt(re.exec(objectName)[1]);
+        lastIndex = 0;
+      }
 
       this.get('source').some((field) => {
         if (field.name === objectName) {
@@ -42,8 +42,16 @@ export default Ember.Component.extend({
         Ember.Logger.debug('dynamic-fields: Adding new field');
         this.get('source').pushObject(Ember.Object.create({name: `${this.get('elementPrefix')}_${1 + lastIndex}`, value: Ember.A()}));
       } else if ((value === undefined) || (value.length === 0)) {
-        Ember.Logger.debug('dynamic-fields: Removing field');
-        this.get('source').removeAt(currentIndex);
+        Ember.Logger.debug(`dynamic-fields: Removing field ${objectName}`);
+
+        let objectToRemove = undefined;
+        this.get('source').some((field) => {
+          if (field.name === objectName) {
+            objectToRemove = field;
+            return true;
+          }
+        });
+        this.get('source').removeObject(objectToRemove);
       }
     }
   }
