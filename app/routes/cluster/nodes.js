e19ce4ef2fd5034e537a0fcf2c4a4527f2b4d827
Marek 'marx' Grác <mgrac@redhat.com>
2018-02-12 11:26:56 +0100

    [fix] /cluster/XX/nodes required auth and it should not

--- app/routes/cluster/nodes.js
+++ app/routes/cluster/nodes.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend({
   redirect(model, transition) {
     if (['cluster.nodes.index', 'cluster.nodes.listing'].indexOf(transition.targetName) >= 0) {
       this.transitionTo('cluster.nodes.show', '');
