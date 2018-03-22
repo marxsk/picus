2ab21ecd4a7b12279f24ba1a81e15aac7cfeb55f
Marek 'marx' Grác <mgrac@redhat.com>
2018-03-22 16:03:24 +0100

    [tests] Remember value in agent-properties after switching to internal names

--- tests/acceptance/fence-test.js
+++ tests/acceptance/fence-test.js
@@ -45,3 +45,14 @@ test('update required field in the form', async (assert) => {
   await click(find('button.form-button--submit')[0]);
   assert.ok(true, 'Update was sent to the back-end');
 });
+
+test('switch prefilled form to internal names', async (assert) => {
+  defaultScenario(server);
+  await visit('/cluster/my/fences/show/Fence-01?tab=properties');
+
+  await fillIn(emberFormForFind('Physical plug number, name of virtual machine or UUID'), '99');
+  assert.equal(find('input')[3].value, 99, 'Plug field is correctly filled');
+  // switch from human-readable to internal-names of properties
+  await click(find('div.x-toggle-btn')[0]);
+  assert.equal(find('input')[3].value, 99, 'Plug field is correctly filled');
+});
