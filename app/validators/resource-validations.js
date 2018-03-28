76aae8cfda33091d485b21bc7291c65488ae9b33
Marek 'marx' Grác <mgrac@redhat.com>
2018-03-28 16:05:41 +0200

    [fix] Remove multiple possible errors for resource name

--- app/validators/resource-validations.js
+++ app/validators/resource-validations.js
@@ -1,8 +1,7 @@
-import { validatePresence, validateLength } from 'ember-changeset-validations/validators';
+import { validatePresence } from 'ember-changeset-validations/validators';
 
 export default {
   resourceName: [
     validatePresence({ presence: true, ignoreBlank: false }),
-    validateLength({ min: 4, allowBlank: false, allowNone: false }),
   ],
 };
